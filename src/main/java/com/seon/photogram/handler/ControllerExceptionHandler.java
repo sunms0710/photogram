655c5365b5ac9793922937c5f2d02ffd29e86bc7
sunms0710 <sunms0710@naver.com>
2023-11-23 02:36:11 +0900

    글로벌 예외처리 개발

--- src/main/java/com/seon/photogram/handler/ControllerExceptionHandler.java
+++ src/main/java/com/seon/photogram/handler/ControllerExceptionHandler.java
@@ -1,18 +1,18 @@
 package com.seon.photogram.handler;
 
 import com.seon.photogram.handler.ex.CustomValidationException;
+import com.seon.photogram.util.Script;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 @ControllerAdvice
 @RestController
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public Map<String, String> validationException(CustomValidationException e) {
-        return e.getErrorMap();
+    public String validationException(CustomValidationException e) {
+        return Script.back(e.getErrorMap().toString());
     }
 }
