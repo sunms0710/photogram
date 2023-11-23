f757bb053f08fe73f4f2b357d87add4adf638f02
sunms0710 <sunms0710@naver.com>
2023-11-24 03:06:24 +0900

    로그인 view 연결

---
+++ src/main/java/com/seon/photogram/web/UserController.java
@@ -0,0 +1,23 @@
+package com.seon.photogram.web;
+
+import com.seon.photogram.config.auth.PrincipalDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/user/{id}")
+    public String profile(@PathVariable int id){
+        return "user/profile";
+    }
+
+    @GetMapping("/user/{id}/update")
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        System.out.println(principalDetails.getUsername());
+        return "user/update";
+    }
+
+}
