56d197e2a35d49778aa9ab9b34e7c6c08fcb344f
sunms0710 <sunms0710@naver.com>
2023-11-24 03:06:42 +0900

    로그인 로직 개발

--- src/main/java/com/seon/photogram/domain/user/UserRepository.java
+++ src/main/java/com/seon/photogram/domain/user/UserRepository.java
@@ -3,4 +3,5 @@ package com.seon.photogram.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
 }
