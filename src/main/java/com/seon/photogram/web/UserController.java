67658fa2fcd4089d327a8c940e6873958135eab9
sunms0710 <sunms0710@naver.com>
2023-11-25 06:35:11 +0900

    코드 수정

--- src/main/java/com/seon/photogram/web/UserController.java
+++ src/main/java/com/seon/photogram/web/UserController.java
@@ -1,7 +1,5 @@
 package com.seon.photogram.web;
 
-import com.seon.photogram.config.auth.PrincipalDetails;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
