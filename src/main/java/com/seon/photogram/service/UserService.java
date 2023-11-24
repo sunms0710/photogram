8f4433f8f363e2a09d1ac61b48e9906c2e08db38
sunms0710 <sunms0710@naver.com>
2023-11-25 06:35:56 +0900

    예외처리 추가

--- src/main/java/com/seon/photogram/service/UserService.java
+++ src/main/java/com/seon/photogram/service/UserService.java
@@ -2,6 +2,7 @@ package com.seon.photogram.service;
 
 import com.seon.photogram.domain.user.User;
 import com.seon.photogram.domain.user.UserRepository;
+import com.seon.photogram.handler.ex.CustomValidationApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,8 @@ public class UserService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     @Transactional
     public User userEdit(int id, User user){
-        User userEntity = userRepository.findById(id).get();
+        User userEntity = userRepository.findById(id).orElseThrow(() ->
+            { return new CustomValidationApiException("찾을 수 없는 id입니다.");});
 
         String rawPassword = user.getPassword();
         String encPassword = bCryptPasswordEncoder.encode(rawPassword);
