303405456c083a05eec6e50c58b57ab1c886c025
sunms0710 <sunms0710@naver.com>
2023-11-25 06:34:55 +0900

    회원정보 수정 api 개발

--- src/main/java/com/seon/photogram/web/api/UserApiController.java
+++ src/main/java/com/seon/photogram/web/api/UserApiController.java
@@ -2,14 +2,21 @@ package com.seon.photogram.web.api;
 
 import com.seon.photogram.config.auth.PrincipalDetails;
 import com.seon.photogram.domain.user.User;
+import com.seon.photogram.handler.ex.CustomValidationApiException;
 import com.seon.photogram.service.UserService;
 import com.seon.photogram.web.dto.CMRespDto;
 import com.seon.photogram.web.dto.user.UserUpdateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +25,21 @@ public class UserApiController {
     private final UserService userService;
 
     @PutMapping("/api/user/{id}")
-    public CMRespDto<?> update(@PathVariable int id, UserUpdateDto userUpdateDto, @AuthenticationPrincipal PrincipalDetails principalDetails) {
-        User userEntity = userService.userEdit(id, userUpdateDto.toEntity());
-        principalDetails.setUser(userEntity);
-        return new CMRespDto<>(1, "회원수정완료", userEntity);
+    public CMRespDto<?> update(@PathVariable int id,
+                               @Valid UserUpdateDto userUpdateDto,
+                               BindingResult bindingResult,
+                               @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        if(bindingResult.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            throw new CustomValidationApiException("유효성 검사 실패", errorMap);
+        } else {
+            User userEntity = userService.userEdit(id, userUpdateDto.toEntity());
+            principalDetails.setUser(userEntity);
+            return new CMRespDto<>(1, "회원수정완료", userEntity);
+        }
     }
 }
