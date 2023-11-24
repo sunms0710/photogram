09afaaf8d425c96e70a8e9aeeaabffa2f7fbb01c
sunms0710 <sunms0710@naver.com>
2023-11-25 06:31:44 +0900

    api 유효성 검사 개발

--- src/main/java/com/seon/photogram/handler/ControllerExceptionHandler.java
+++ src/main/java/com/seon/photogram/handler/ControllerExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.seon.photogram.handler;
 
+import com.seon.photogram.handler.ex.CustomValidationApiException;
 import com.seon.photogram.handler.ex.CustomValidationException;
 import com.seon.photogram.util.Script;
+import com.seon.photogram.web.dto.CMRespDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +18,10 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(CustomValidationException.class)
     public String validationException(CustomValidationException e) {
         return Script.back(e.getErrorMap().toString());
+    }
+
+    @ExceptionHandler(CustomValidationApiException.class)
+    public ResponseEntity<CMRespDto<?>> validationApiException(CustomValidationApiException e) {
+        return new ResponseEntity<>(new CMRespDto<>(-1, e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
     }
 }
