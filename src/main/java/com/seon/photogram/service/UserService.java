24c1495089e7a8e9ffc41499d5ae5bcf6395ad24
sunms0710 <sunms0710@naver.com>
2023-11-25 03:20:12 +0900

    회원정보 수정 service 개발

---
+++ src/main/java/com/seon/photogram/service/UserService.java
@@ -0,0 +1,32 @@
+package com.seon.photogram.service;
+
+import com.seon.photogram.domain.user.User;
+import com.seon.photogram.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Transactional
+    public User userEdit(int id, User user){
+        User userEntity = userRepository.findById(id).get();
+
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+
+        userEntity.setName(user.getName());
+        userEntity.setPassword(encPassword);
+        userEntity.setBio(user.getBio());
+        userEntity.setWebsite(user.getWebsite());
+        userEntity.setPhone(user.getPhone());
+        userEntity.setGender(user.getGender());
+
+        return userEntity;
+    }
+}
