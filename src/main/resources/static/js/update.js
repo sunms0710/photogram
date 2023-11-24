a18c83cb52546fd9b2dad4f4210a8acbb1ce62e5
sunms0710 <sunms0710@naver.com>
2023-11-25 06:32:54 +0900

    회원정보 수정 ajax 통신 개발

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,5 +1,6 @@
 //회원정보 수정
-function update(userId) {
+function update(userId,event) {
+    event.preventDefault();
     let data = $("#profileUpdate").serialize();
     $.ajax({
         type: "put",
@@ -8,9 +9,13 @@ function update(userId) {
         contentType: "application/x-www-form-urlencoded; charset=utf-8",
         dataType: "json"
     }).done(res => {
-        console.log("업데이트 성공");
+        console.log("업데이트 성공", res);
         location.href=`/user/${userId}`;
     }).fail(error => {
-        console.log("업데이트 실패");
+        if(error.data == null) {
+            alert(error.responseJSON.message);
+        }else {
+            alert(JSON.stringify(error.responseJSON.data));
+        }
     });
 }
