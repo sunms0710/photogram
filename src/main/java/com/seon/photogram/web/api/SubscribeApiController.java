27b64cb3ee3baaccbc338b5551fb8222979e7634
sunms0710 <sunms0710@naver.com>
2023-11-26 02:58:37 +0900

    구독,구독취소 api 개발

---
+++ src/main/java/com/seon/photogram/web/api/SubscribeApiController.java
@@ -0,0 +1,32 @@
+package com.seon.photogram.web.api;
+
+import com.seon.photogram.config.auth.PrincipalDetails;
+import com.seon.photogram.service.SubscribeService;
+import com.seon.photogram.web.dto.CMRespDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SubscribeApiController {
+
+    private final SubscribeService subscribeService;
+
+    @PostMapping("/api/subscribe/{toUserId}")
+    public ResponseEntity<?> subscribe(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable int toUserId){
+        subscribeService.subscribe(principalDetails.getUser().getId(), toUserId);
+        return new ResponseEntity<>(new CMRespDto<>(1, "구독성공", null), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api/subscribe/{toUserId}")
+    public ResponseEntity<?> unSubscribe(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable int toUserId){
+        subscribeService.unSubscribe(principalDetails.getUser().getId(), toUserId);
+        return new ResponseEntity<>(new CMRespDto<>(1, "구독취소성공", null), HttpStatus.OK);
+    }
+}
