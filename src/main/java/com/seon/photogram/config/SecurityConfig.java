146f875a6dbbd8d4815b379eae2b27af8d8f5514
sunms0710 <sunms0710@naver.com>
2023-11-26 02:57:58 +0900

    SecurityConfig 수정

--- src/main/java/com/seon/photogram/config/SecurityConfig.java
+++ src/main/java/com/seon/photogram/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/","/user/**","/image/**","/subscribe/**","/comment/**").authenticated()
+                .antMatchers("/","/user/**","/image/**","/subscribe/**","/comment/**", "/api/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
