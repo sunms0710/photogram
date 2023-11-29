36f10eabb0dc2f7f685d5697516dfa7a17aa77eb
sunms0710 <sunms0710@naver.com>
2023-11-29 23:10:53 +0900

    양방향 매핑 추가

--- src/main/java/com/seon/photogram/domain/user/User.java
+++ src/main/java/com/seon/photogram/domain/user/User.java
@@ -1,6 +1,8 @@
 package com.seon.photogram.domain.user;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.seon.photogram.domain.image.Image;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +10,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @NoArgsConstructor
@@ -40,6 +43,14 @@ public class User {
 
     private String profileImageUrl;
     private String role;
+
+    // mappedBy: 연관관계의 주인이 아님, 데이터 칼럼 안만듬
+    // User를 Select할 때 해당 id로 등록된 image를 가져옴
+    // LAZY: User를 Select할 때 해당 id로 등록된 image를 가져오지않는대신 get함수를 호출할 때 가져옴
+    // EAGER: User를 Select할 때 해당 id로 등록된 image를 전부 Join해서 가져옴
+    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"user"}) //무한참조 방지
+    private List<Image> images; //양방향 매핑
 
     private LocalDateTime createDate;
 
