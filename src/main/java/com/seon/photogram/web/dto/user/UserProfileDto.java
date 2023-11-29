0bbf7e16e8829aa513be4e0b378428ba11108ca6
sunms0710 <sunms0710@naver.com>
2023-11-29 23:12:40 +0900

    회원 프로필 dto 개발

---
+++ src/main/java/com/seon/photogram/web/dto/user/UserProfileDto.java
@@ -0,0 +1,17 @@
+package com.seon.photogram.web.dto.user;
+
+import com.seon.photogram.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserProfileDto {
+    private boolean pageOwnerState;
+    private int imageCount;
+    private User user;
+}
