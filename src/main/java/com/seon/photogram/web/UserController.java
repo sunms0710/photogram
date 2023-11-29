21c58e52dda7e99093d88640689701cf6683724f
sunms0710 <sunms0710@naver.com>
2023-11-29 23:12:17 +0900

    회원 정보 담기

--- src/main/java/com/seon/photogram/web/UserController.java
+++ src/main/java/com/seon/photogram/web/UserController.java
@@ -1,14 +1,25 @@
 package com.seon.photogram.web;
 
+import com.seon.photogram.config.auth.PrincipalDetails;
+import com.seon.photogram.service.UserService;
+import com.seon.photogram.web.dto.user.UserProfileDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 
-    @GetMapping("/user/{id}")
-    public String profile(@PathVariable int id){
+    private final UserService userService;
+
+    @GetMapping("/user/{pageUserId}")
+    public String profile(@PathVariable int pageUserId, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        UserProfileDto userProfileDto = userService.userProfile(pageUserId, principalDetails.getUser().getId());
+        model.addAttribute("userProfileDto", userProfileDto);
         return "user/profile";
     }
 
