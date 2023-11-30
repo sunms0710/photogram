ea4e785964178e09908b1b47d40f26c5ad9768f1
sunms0710 <sunms0710@naver.com>
2023-11-30 18:37:26 +0900

    구독상태, 구독자수 nativeQuery 개발

--- src/main/java/com/seon/photogram/domain/subscribe/SubScribeRepository.java
+++ src/main/java/com/seon/photogram/domain/subscribe/SubScribeRepository.java
@@ -6,11 +6,17 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface SubScribeRepository extends JpaRepository<Subscribe, Integer> {
 
-    @Modifying
+    @Modifying // INSERT, DELETE, UPDATE를 nativeQuery로 작성하려면 필요
     @Query(value = "INSERT INTO subscribe(fromUserId, toUserId, createDate) VALUES(:fromUserId, :toUserId, now())", nativeQuery = true)
     void mSubscribe(int fromUserId, int toUserId);
 
     @Modifying
     @Query(value = "DELETE FROM subscribe WHERE fromUserId = :fromUserId AND toUserId = :toUserId", nativeQuery = true)
     void mUnSubscribe(int fromUserId, int toUserId);
+
+    @Query(value = "SELECT COUNT(*) FROM subscribe WHERE fromUserId = :principalId AND toUserId = :pageUserId", nativeQuery = true)
+    int mSubscribeState(int principalId, int pageUserId);
+
+    @Query(value = "SELECT COUNT(*) FROM subscribe WHERE fromUserId = :pageUserId", nativeQuery = true)
+    int mSubscribeCount(int pageUserId);
 }
