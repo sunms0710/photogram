e288480835d618f3b39b12274d0757b76215c876
sunms0710 <sunms0710@naver.com>
2023-12-02 00:51:00 +0900

    구독 정보 service 개발

--- src/main/java/com/seon/photogram/service/SubscribeService.java
+++ src/main/java/com/seon/photogram/service/SubscribeService.java
@@ -4,9 +4,12 @@ import com.seon.photogram.domain.subscribe.SubScribeRepository;
 import com.seon.photogram.handler.ex.CustomApiException;
 import com.seon.photogram.web.dto.subscribe.SubscribeDto;
 import lombok.RequiredArgsConstructor;
+import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Service
@@ -14,6 +17,7 @@ import java.util.List;
 public class SubscribeService {
 
     private final SubScribeRepository subScribeRepository;
+    private final EntityManager em;
 
     @Transactional
     public void subscribe(int fromUserId, int toUserId){
@@ -31,7 +35,26 @@ public class SubscribeService {
     }
 
     @Transactional(readOnly = true)
-    public List<SubscribeDto> subscribeList(int principalUserId, int pageUserId) {
-        return null;
+    public List<SubscribeDto> subscribeList(int principalId, int pageUserId) {
+        // 쿼리 준비
+        StringBuffer sb = new StringBuffer();
+        sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
+        sb.append("if ((SELECT 1 FROM subscribe WHERE fromUserId = ? AND toUserId = u.id), 1, 0) subscribeState, "); // 물음표 principalId
+        sb.append("if ((?=u.id), 1, 0) equalUserState "); // 물음표 principalId
+        sb.append("FROM user u INNER JOIN subscribe s ");
+        sb.append("ON u.id = s.toUserId ");
+        sb.append("WHERE s.fromUserId = ?"); // 물음표 pageUserId
+
+        // 쿼리 완성
+        Query query = em.createNativeQuery(sb.toString())
+                .setParameter(1, principalId)
+                .setParameter(2, principalId)
+                .setParameter(3, pageUserId);
+
+        // 쿼리 실행 (qlrm 라이브러리 필요 = Dto에 DB결과를 매핑하기 위해서)
+        JpaResultMapper result = new JpaResultMapper();
+        List<SubscribeDto> subscribeDtos = result.list(query, SubscribeDto.class);
+        return subscribeDtos;
     }
+
 }
