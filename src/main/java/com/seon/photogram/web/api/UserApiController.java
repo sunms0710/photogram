7762d57b5f4b690744231959bd65f73ac72cc1c9
sunms0710 <sunms0710@naver.com>
2023-12-01 22:46:47 +0900

    구독 정보 api 개발

--- src/main/java/com/seon/photogram/web/api/UserApiController.java
+++ src/main/java/com/seon/photogram/web/api/UserApiController.java
@@ -3,19 +3,25 @@ package com.seon.photogram.web.api;
 import com.seon.photogram.config.auth.PrincipalDetails;
 import com.seon.photogram.domain.user.User;
 import com.seon.photogram.handler.ex.CustomValidationApiException;
+import com.seon.photogram.service.SubscribeService;
 import com.seon.photogram.service.UserService;
 import com.seon.photogram.web.dto.CMRespDto;
+import com.seon.photogram.web.dto.subscribe.SubscribeDto;
 import com.seon.photogram.web.dto.user.UserUpdateDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,6 +29,13 @@ import java.util.Map;
 public class UserApiController {
 
     private final UserService userService;
+    private final SubscribeService subscribeService;
+
+    @GetMapping("/api/user/{pageUserId}/subscribe")
+    public ResponseEntity<?> subscribeList(@PathVariable int pageUserId, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        List<SubscribeDto> subscribeDto = subscribeService.subscribeList(principalDetails.getUser().getId(), pageUserId);
+        return new ResponseEntity<>(new CMRespDto<>(1,"구독자 정보 리스트 불러오기 성공", subscribeDto), HttpStatus.OK);
+    }
 
     @PutMapping("/api/user/{id}")
     public CMRespDto<?> update(@PathVariable int id,
