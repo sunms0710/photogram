0f9ed2ebd4a31be5cd7f5ab644864e6552771cb4
sunms0710 <sunms0710@naver.com>
2023-12-05 02:06:55 +0900

    포토리스트 api 개발

---
+++ src/main/java/com/seon/photogram/web/api/ImageApiController.java
@@ -0,0 +1,27 @@
+package com.seon.photogram.web.api;
+
+import com.seon.photogram.config.auth.PrincipalDetails;
+import com.seon.photogram.domain.image.Image;
+import com.seon.photogram.service.ImageService;
+import com.seon.photogram.web.dto.CMRespDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ImageApiController {
+
+    private final ImageService imageService;
+
+    @GetMapping("/api/image")
+    public ResponseEntity<?> imageStory(@AuthenticationPrincipal PrincipalDetails principalDetails){
+        List<Image> images = imageService.imageStory(principalDetails.getUser().getId());
+        return new ResponseEntity<>(new CMRespDto<>(1, "성공", images), HttpStatus.OK);
+    }
+}
