c1420eeed1864c55b81e1033c16a4fce6a88f967
sunms0710 <sunms0710@naver.com>
2023-12-05 02:07:27 +0900

    포토리스트 nativeQuery 구현

--- src/main/java/com/seon/photogram/domain/image/ImageRepository.java
+++ src/main/java/com/seon/photogram/domain/image/ImageRepository.java
@@ -1,6 +1,12 @@
 package com.seon.photogram.domain.image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+
+    @Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe WHERE fromUserId = :principalId)", nativeQuery = true)
+    List<Image> mStory(int principalId);
 }
