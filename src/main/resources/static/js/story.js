4204e91d5d15986b0344428aad4e552e5c769a4e
sunms0710 <sunms0710@naver.com>
2023-12-06 02:48:43 +0900

    스크롤 페이징 로딩 구현

--- src/main/resources/static/js/story.js
+++ src/main/resources/static/js/story.js
@@ -8,13 +8,15 @@
  */
 
 // (1) 스토리 로드하기
+let page = 0;
+
 function storyLoad() {
 	$.ajax({
-		url: `/api/image`,
+		url: `/api/image?page=${page}`,
 		dataType: "json"
 	}).done(res => {
 		console.log(res);
-		res.data.forEach((image) => {
+		res.data.content.forEach((image) => {
 			let storyItem = getStoryItem(image);
 			$("#storyList").append(storyItem);
 		});
@@ -75,7 +77,11 @@ function getStoryItem(image) {
 
 // (2) 스토리 스크롤 페이징하기
 $(window).scroll(() => {
-
+	let checkNum = $(window).scrollTop() - ($(document).height() - $(window).height());
+	if(checkNum < 1 && checkNum > -1) {
+		page++;
+		storyLoad();
+	}
 });
 
 
