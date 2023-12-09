b3cd15bbc927a4ea949a52eb0e6b6556d37cd6c0
sunms0710 <sunms0710@naver.com>
2023-12-10 04:05:27 +0900

    좋아요 model 개발

---
+++ src/main/java/com/seon/photogram/domain/likes/Likes.java
@@ -0,0 +1,47 @@
+package com.seon.photogram.domain.likes;
+
+import com.seon.photogram.domain.image.Image;
+import com.seon.photogram.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "likes_uk",
+                        columnNames = {"imageId", "userId"}
+                )
+        }
+)
+public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name = "imageId")
+    @ManyToOne
+    private Image image;
+
+
+    @JoinColumn(name = "userId")
+    @ManyToOne
+    private User user;
+
+    private LocalDateTime createDate;
+
+    @PrePersist
+    public void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+}
