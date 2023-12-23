a3e831a2f4b1cef5e3d0b8350abb31ca157ca391
sunms0710 <sunms0710@naver.com>
2023-12-23 18:53:16 +0900

    유저 프로필 사진 변경 구현

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -78,7 +78,11 @@ function getSubscribeModalItem(u) {
 }
 
 // (3) 유저 프로파일 사진 변경 (완)
-function profileImageUpload() {
+function profileImageUpload(pageUserId, principalId) {
+	if(pageUserId != principalId) {
+		alert("권한이 없습니다.")
+		return;
+	}
 	$("#userProfileImageInput").click();
 
 	$("#userProfileImageInput").on("change", (e) => {
@@ -89,12 +93,32 @@ function profileImageUpload() {
 			return;
 		}
 
-		// 사진 전송 성공시 이미지 변경
-		let reader = new FileReader();
-		reader.onload = (e) => {
-			$("#userProfileImage").attr("src", e.target.result);
-		}
-		reader.readAsDataURL(f); // 이 코드 실행시 reader.onload 실행됨.
+		// 서버에 이미지를 전송
+		let profileImageForm = $("#userProfileImageForm")[0];
+
+		// FormData 객체를 이용하면 form 태그의 필드와 그 값을 나타내는 일련의 key/value 쌍을 담을 수 있다
+		let formData = new FormData(profileImageForm)
+
+		$.ajax({
+			type:"put",
+			url:`/api/user/${principalId}/profileImageUrl`,
+			data: formData,
+			contentType: false, // x-www-form-urlencoded로 파싱되는것을 방지
+			processData: false, // contentType을 false로 줬을 때 QueryString 자동 설정되는것을 방지
+			enctype: "multipart/form-data",
+			dataType: "json"
+		}).done(res =>{
+			// 사진 전송 성공시 이미지 변경
+			let reader = new FileReader();
+			reader.onload = (e) => {
+				$("#userProfileImage").attr("src", e.target.result);
+			}
+			reader.readAsDataURL(f); // 이 코드 실행시 reader.onload 실행됨.
+		}).fail(error=>{
+			console.log("오류", error)
+		});
+
+
 	});
 }
 
