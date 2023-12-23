dfdcfe0bd82dfe0614f6487bb425cea8727a0243
sunms0710 <sunms0710@naver.com>
2023-12-24 02:21:31 +0900

    댓글 model 개발

---
+++ src/main/java/com/seon/photogram/domain/comment/Comment.java
@@ -0,0 +1,40 @@
+package com.seon.photogram.domain.comment;
+
+import com.seon.photogram.domain.image.Image;
+import com.seon.photogram.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 100, nullable = false)
+    private String content;
+
+    @JoinColumn(name = "userID")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+
+    @JoinColumn(name = "imageId")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Image image;
+
+    private LocalDateTime createDate;
+
+    @PrePersist
+    public void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+}
