eec6e8d465a908bf02e56a7feedb0c3694cecc5b
sunms0710 <sunms0710@naver.com>
2023-12-26 16:49:19 +0900

    댓글쓰기 api 개발

--- src/main/java/com/seon/photogram/web/api/CommentApiController.java
+++ src/main/java/com/seon/photogram/web/api/CommentApiController.java
@@ -1,12 +1,15 @@
 package com.seon.photogram.web.api;
 
+import com.seon.photogram.config.auth.PrincipalDetails;
+import com.seon.photogram.domain.comment.Comment;
 import com.seon.photogram.service.CommentService;
+import com.seon.photogram.web.dto.CMRespDto;
+import com.seon.photogram.web.dto.comment.CommentDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,8 +19,9 @@ public class CommentApiController {
 
     // 댓글 쓰기
     @PostMapping("/api/comment")
-    public ResponseEntity<?> commentWrite() {
-        return null;
+    public ResponseEntity<?> commentWrite(@RequestBody CommentDto commentDto, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Comment comment = commentService.commentWrite(commentDto.getContent(), commentDto.getImageId(), principalDetails.getUser().getId());
+        return new ResponseEntity<>(new CMRespDto<>(1, "댓글쓰기성공", comment), HttpStatus.CREATED);
     }
 
     // 댓글 삭제
