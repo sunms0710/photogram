30c1dcca3b6cf6c1ad4d1cfd1994c52254c53608
sunms0710 <sunms0710@naver.com>
2024-01-01 16:34:08 +0900

    username 길이 변경

--- src/main/java/com/seon/photogram/domain/user/User.java
+++ src/main/java/com/seon/photogram/domain/user/User.java
@@ -23,7 +23,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(length = 20, unique = true)
+    @Column(length = 100, unique = true)
     private String username;
 
     @Column(nullable = false)
