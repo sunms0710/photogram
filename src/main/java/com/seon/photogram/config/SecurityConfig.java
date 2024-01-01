8025622a13d5e4663e9d5eb0a89a842dc2750e24
sunms0710 <sunms0710@naver.com>
2024-01-01 16:33:07 +0900

    OAuth2 페이스북 로그인 config 설정

--- src/main/java/com/seon/photogram/config/SecurityConfig.java
+++ src/main/java/com/seon/photogram/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.seon.photogram.config;
 
+import com.seon.photogram.config.oauth.Oauth2DetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,10 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final Oauth2DetailsService oauth2DetailsService;
     @Bean
     public BCryptPasswordEncoder encode() {
         return new BCryptPasswordEncoder();
@@ -26,6 +30,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/auth/signin") //get
                 .loginProcessingUrl("/auth/signin") //post
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint() // oauth2 로그인을 하면 최종 응답으로 회원정보를 바로 받을 수 있다.
+                .userService(oauth2DetailsService);
     }
 }
